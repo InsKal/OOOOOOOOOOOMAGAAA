0d742466eb155755d69c6e80dd90c060656f1343
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-04-20 22:00:32 -0700

    my bad

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -308,8 +308,9 @@ class GreyscaleShader extends FlxShader{
 //fixed this shit 
 class GrainEffect extends Effect {
 	
-	public var shader: GrainShader = new GrainShader();
-	public function new (grainsize, lumamount,lockAlpha,coloramount){
+        public var shader:Grain;
+	public function new (grainsize, lumamount,lockAlpha,coloramount){ 
+		shader = new Grain();
 		shader.data.lumamount.value = [lumamount];
 		shader.data.grainsize.value = [grainsize];
 		shader.data.lockAlpha.value = [lockAlpha]; 
