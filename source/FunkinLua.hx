7b106f15660a25869e2f565469cd5814ca16f869
jonnycat <82131956+McJonnycat@users.noreply.github.com>
2022-04-28 17:42:13 -0700

    no no error

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1651,13 +1651,6 @@ class FunkinLua {
 			PlayState.instance.addShaderToCamera(camera, new VCRDistortionEffect(glitchFactor,distortion,perspectiveOn,vignetteMoving));
 			
 		});
-		//test		
-		 Lua_helper.add_callback(lua, "addBlammedEffect", function(camera:String,r:Float,g:Float,b:Float,enabled:Bool ) {
-			
-		        PlayState.instance.addShaderToCamera(camera, new BlammedEffect(r,g,b,enabled));
-			
-		}); 
-		//r:Float,g:Float,b:Float,enabled:Bool 
 		
 Lua_helper.add_callback(lua, "createShaders", function(?shaderName:String, ?optimize:Bool = false)
 {
